83dfbc7298f91c3d10837efd6c1cfc4442382c4e
Alexey Karnachev <alekseykarnachev@gmail.com>
2024-03-04 01:32:56 +0400

    add third person controller

--- bin/game.cpp
+++ bin/game.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "rcamera.h"
+#include "rlgl.h"
 #include <cstdint>
 #include <cstdio>
 #include <string>
@@ -33,19 +34,19 @@ class Resources {
     }
 };
 
-class GameCamera {
+class ThirdPersonCamera {
   public:
     Camera3D camera3d;
 
-    GameCamera(Vector3 target) {
+    ThirdPersonCamera(Vector3 target) {
         camera3d.target = target;
-        camera3d.position = (Vector3){0.0, 5.0, -5.0};
+        camera3d.position = (Vector3){0.0, 3.0, -3.0};
         camera3d.fovy = 70.0f;
         camera3d.projection = CAMERA_PERSPECTIVE;
         camera3d.up = {0.0f, 1.0f, 0.0f};
     }
 
-    void update_third_person(Vector3 target) {
+    void update(Vector3 target) {
         static float rot_speed = 0.003f;
 
         Vector3 translation = Vector3Subtract(target, camera3d.target);
@@ -54,13 +55,51 @@ class GameCamera {
 
         Vector2 mouse_delta = GetMouseDelta();
         CameraYaw(&camera3d, -rot_speed * mouse_delta.x, true);
-        CameraPitch(&camera3d, rot_speed * mouse_delta.y, true, true, false);
+        CameraPitch(&camera3d, -rot_speed * mouse_delta.y, true, true, false);
+    }
+};
+
+class ThirdPersonController {
+  public:
+    ThirdPersonController(){};
+
+    Vector3 get_step(float dt, Camera3D camera3d, Vector3 position) {
+        Vector2 dir = Vector2Zero();
+        if (IsKeyDown(KEY_W)) dir.y -= 1.0;
+        if (IsKeyDown(KEY_S)) dir.y += 1.0;
+        if (IsKeyDown(KEY_A)) dir.x -= 1.0;
+        if (IsKeyDown(KEY_D)) dir.x += 1.0;
+
+        if (dir.x != 0.0 || dir.y != 0.0) {
+            Vector2 s = GetWorldToScreen(position, camera3d);
+            s = Vector2Add(s, dir);
+            Ray r = GetMouseRay(s, camera3d);
+
+            // Intersect line and plane
+            Vector3 plane_normal = {0.0, 1.0, 0.0};
+            float dot = Vector3DotProduct(r.direction, plane_normal);
+            if (fabs(dot) > EPSILON) {
+                Vector3 w = Vector3Subtract(position, r.position);
+                float t = Vector3DotProduct(plane_normal, w) / dot;
+                Vector3 isect = Vector3Add(
+                    r.position, Vector3Scale(Vector3Normalize(r.direction), t)
+                );
+
+                Vector3 d = Vector3Subtract(isect, position);
+                d = Vector3Normalize(d);
+                Vector3 step = Vector3Scale(d, dt * 1.5);
+                return step;
+            }
+        }
+
+        return Vector3Zero();
     }
 };
 
 class Player {
   public:
-    GameCamera camera;
+    ThirdPersonCamera camera;
+    ThirdPersonController controller;
     Transform transform;
 
     Player(Vector3 position) : camera(position) {
@@ -69,8 +108,11 @@ class Player {
         transform.translation = position;
     }
 
-    void update() {
-        camera.update_third_person(transform.translation);
+    void update(float dt) {
+        Vector3 step = controller.get_step(dt, camera.camera3d, transform.translation);
+        transform.translation = Vector3Add(transform.translation, step);
+
+        camera.update(transform.translation);
     }
 };
 
@@ -81,7 +123,7 @@ class World {
     World(Vector3 player_position) : player(player_position) {}
 
     void update() {
-        player.update();
+        player.update(GetFrameTime());
     }
 };
 
@@ -99,9 +141,6 @@ class Renderer {
     }
 
     void draw_world(World &world, Resources &resources) {
-        BeginDrawing();
-        ClearBackground(DARKBLUE);
-
         BeginMode3D(world.player.camera.camera3d);
         DrawModel(
             resources.models["player"], world.player.transform.translation, 0.01, WHITE
@@ -109,8 +148,6 @@ class Renderer {
         DrawGrid(16, 1.0);
         EndMode3D();
         DrawFPS(0, 0);
-
-        EndDrawing();
     }
 };
 
@@ -120,8 +157,13 @@ int main(void) {
     Resources resources;
 
     while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(DARKBLUE);
+
         world.update();
         renderer.draw_world(world, resources);
+
+        EndDrawing();
     }
 
     return 0;
