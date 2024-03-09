1fc04841568841e915d12727f713c08af74f11f9
Alexey Karnachev <alekseykarnachev@gmail.com>
2024-03-10 01:25:56 +0400

    add sprite sheet and animator

--- bin/game.cpp
+++ bin/game.cpp
@@ -1,295 +1,135 @@
+#include "json.hpp"
 #include "raylib.h"
-#include "raymath.h"
-#include "rcamera.h"
-#include "rlgl.h"
 #include <algorithm>
 #include <cmath>
-#include <cstdint>
-#include <cstdio>
+#include <fstream>
 #include <iostream>
-#include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <vector>
+using json = nlohmann::json;
 
-using u32 = uint32_t;
-using u16 = uint16_t;
-using u8 = uint8_t;
+Rectangle rect_from_json(json data) {
+    Rectangle rect = {
+        .x = data["x"], .y = data["y"], .width = data["w"], .height = data["h"]};
 
-#define print_vec3(v) (printf("%f, %f, %f\n", v.x, v.y, v.z))
-#define print_vec2(v) (printf("%f, %f\n", v.x, v.y))
+    return rect;
+}
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
-
-class AnimatedModel {
+class Sprite {
   public:
-    Model model;
-    ModelAnimation *animations;
+    Texture2D texture;
+    Rectangle src;
+    std::unordered_map<std::string, Rectangle> masks;
 
-    int animation_idx = 0;
-    int n_animations = 0;
-    int n_frames = 0;
-    int fps = 0;
-    int frame_idx = 0;
-    float time = 0.0;
+    Sprite(
+        Texture2D texture, Rectangle src, std::unordered_map<std::string, Rectangle> masks
+    )
+        : texture(texture), src(src), masks(masks) {}
 
-    AnimatedModel() {}
+    Sprite(json frame_json, Texture2D texture) : texture(texture) {
+        json sprite_json = frame_json["sprite"];
+        json masks_json = frame_json["masks"];
+        this->src = rect_from_json(sprite_json);
 
-    AnimatedModel(const char *file_path) {
-        model = LoadModel(file_path);
-        animations = LoadModelAnimations(file_path, &n_animations);
-        if (n_animations == 0) return;
-
-        n_frames = animations[0].frameCount;
-        fps = 30;
-    }
-
-    void play(std::string animation_name, int fps, float dt) {
-        this->fps = fps;
-
-        int animation_idx = -1;
-        for (int i = 0; i < n_animations; ++i) {
-            if (animation_name == animations[i].name) {
-                animation_idx = i;
-            }
+        std::unordered_map<std::string, Rectangle> masks;
+        for (auto it = masks_json.begin(); it != masks_json.end(); ++it) {
+            const auto &mask_name = it.key();
+            const auto &mask_json = it.value();
+            this->masks[mask_name] = rect_from_json(mask_json);
         }
-
-        if (animation_idx == -1) {
-            throw std::runtime_error("Animation " + animation_name + " is not found");
-        }
-
-        if (this->animation_idx != animation_idx) {
-            this->animation_idx = animation_idx;
-            n_frames = animations[this->animation_idx].frameCount;
-            time = 0.0;
-        }
-
-        if (n_frames <= 0) {
-            throw std::runtime_error(
-                "Can't update the AnimationCounter with n_frames <= 0"
-            );
-        }
-
-        time += dt;
-        frame_idx = time / (1.0f / fps);
-        frame_idx %= n_frames;
-    }
-
-    void draw() {
-        UpdateModelAnimation(model, animations[animation_idx], frame_idx);
-        DrawModel(model, Vector3Zero(), 0.01, WHITE);
     }
 };
 
-class Resources {
-  public:
-    std::unordered_map<std::string, Shader> shaders;
-    std::unordered_map<std::string, AnimatedModel> animated_models;
-
-    Resources() {
-        animated_models["zed_1"] = AnimatedModel("resources/models/zed_1.glb");
-    }
-
-    ~Resources() {
-        for (auto &pair : shaders)
-            UnloadShader(pair.second);
-        for (auto &pair : animated_models)
-            UnloadModelAnimations(pair.second.animations, pair.second.n_animations);
-    }
-};
-
-class ThirdPersonCamera {
+class SpriteSheet {
   private:
-    float min_y = 0.2;
-    float dist_to_target = 5.0;
+    Texture2D texture;
+    json meta;
 
   public:
-    Camera3D camera3d;
-
-    ThirdPersonCamera(Vector3 target) {
-        camera3d.target = target;
-        camera3d.position = (Vector3){0.0, 3.0, -3.0};
-        camera3d.fovy = 70.0f;
-        camera3d.projection = CAMERA_PERSPECTIVE;
-        camera3d.up = {0.0f, 1.0f, 0.0f};
-    }
-
-    void update(Vector3 target) {
-        static float rot_speed = 0.003f;
-
-        Vector3 translation = Vector3Subtract(target, camera3d.target);
-        camera3d.position = Vector3Add(translation, camera3d.position);
-        camera3d.target = target;
-
-        Vector2 mouse_delta = GetMouseDelta();
-        CameraYaw(&camera3d, -rot_speed * mouse_delta.x, true);
-        CameraPitch(&camera3d, -rot_speed * mouse_delta.y, true, true, false);
-
-        camera3d.position.y = std::max(camera3d.position.y, min_y);
-        CameraMoveToTarget(
-            &camera3d, dist_to_target - Vector3Distance(target, camera3d.position)
-        );
-    }
-};
-
-class ThirdPersonController {
-  private:
-    Vector3 world_dir;
-    u8 keys_mask;
-    bool preserve_direction = false;
-
-  public:
-    ThirdPersonController() : keys_mask(0){};
-
-    Vector3 get_direction(Camera3D camera3d, Vector3 position) {
-        u8 new_keys_mask = 0;
-        new_keys_mask |= IsKeyDown(KEY_W) << 0;
-        new_keys_mask |= IsKeyDown(KEY_S) << 1;
-        new_keys_mask |= IsKeyDown(KEY_A) << 2;
-        new_keys_mask |= IsKeyDown(KEY_D) << 3;
-
-        if (!new_keys_mask) {
-            world_dir = Vector3Zero();
-        } else if (new_keys_mask && (new_keys_mask != keys_mask || !preserve_direction)) {
-            Vector2 dir = Vector2Zero();
-            if (IsKeyDown(KEY_W)) dir.y -= 1.0;
-            if (IsKeyDown(KEY_S)) dir.y += 1.0;
-            if (IsKeyDown(KEY_A)) dir.x -= 1.0;
-            if (IsKeyDown(KEY_D)) dir.x += 1.0;
-
-            Vector2 s = GetWorldToScreen(position, camera3d);
-            s = Vector2Add(s, dir);
-            Ray r = GetMouseRay(s, camera3d);
-
-            // Intersect line and plane
-            Vector3 plane_normal = {0.0, 1.0, 0.0};
-            float dot = Vector3DotProduct(r.direction, plane_normal);
-            if (fabs(dot) > EPSILON) {
-                Vector3 w = Vector3Subtract(position, r.position);
-                float t = Vector3DotProduct(plane_normal, w) / dot;
-                Vector3 isect = Vector3Add(
-                    r.position, Vector3Scale(Vector3Normalize(r.direction), t)
-                );
-
-                world_dir = Vector3Normalize(Vector3Subtract(isect, position));
-                world_dir.y = 0.0;
-            }
+    SpriteSheet(std::string meta_file_path, std::string texture_file_path) {
+        std::ifstream file(meta_file_path);
+        if (!file.is_open()) {
+            throw std::runtime_error("Failed to open file: " + meta_file_path);
         }
 
-        keys_mask = new_keys_mask;
-        return world_dir;
+        meta = json::parse(file);
+        file.close();
+
+        texture = LoadTexture(texture_file_path.c_str());
+    }
+
+    ~SpriteSheet() {
+        UnloadTexture(texture);
+    }
+
+    int count_frames(std::string &name) {
+        return meta["frames"][name].size();
+    }
+
+    Sprite get_sprite(std::string &name, int idx) {
+        json frame_json = meta["frames"][name][idx];
+        Sprite sprite(frame_json, this->texture);
+        return sprite;
     }
 };
 
-enum class PlayerState {
-    IDLE,
-    WALK,
-};
+class SpriteSheetAnimator {
+  private:
+    SpriteSheet &sprite_sheet;
+    std::string name = "";
+    float frame_duration = 0.0;
+    float progress = 0.0;
+    bool is_repeat = true;
 
-class Player {
   public:
-    ThirdPersonCamera camera;
-    ThirdPersonController controller;
-    Transform transform;
-    PlayerState state;
-    AnimatedModel animated_model;
+    SpriteSheetAnimator(SpriteSheet &sprite_sheet) : sprite_sheet(sprite_sheet) {}
 
-    Player(Vector3 position, AnimatedModel animated_model)
-        : camera(position), animated_model(animated_model) {
-        transform.rotation = QuaternionIdentity();
-        transform.scale = Vector3One();
-        transform.translation = position;
-        state = PlayerState::IDLE;
+    void play(std::string name, float frame_duration, bool is_repeat) {
+        this->frame_duration = frame_duration;
+        this->is_repeat = is_repeat;
+
+        if (this->name != name) {
+            this->name = name;
+            this->progress = 0.0;
+        }
     }
 
     void update(float dt) {
-        state = PlayerState::IDLE;
+        if (this->name.empty()) return;
 
-        Vector3 dir = controller.get_direction(camera.camera3d, transform.translation);
-        if (Vector3Length(dir) > EPSILON) {
-            transform.translation = Vector3Add(
-                transform.translation, Vector3Scale(dir, dt * 1.0)
-            );
-            transform.rotation = QuaternionFromVector3ToVector3(
-                (Vector3){0.0, 0.0, 1.0}, Vector3Normalize(dir)
-            );
+        int n_frames = this->sprite_sheet.count_frames(this->name);
 
-            state = PlayerState::WALK;
-        }
+        this->progress += dt / (n_frames * this->frame_duration);
+        if (this->is_repeat) this->progress -= std::floor(this->progress);
+        else this->progress = std::fmin(this->progress, 1.0);
+    }
 
-        camera.update(transform.translation);
-
-        if (state == PlayerState::IDLE) {
-            animated_model.play("idle", 120, dt);
-        } else if (state == PlayerState::WALK) {
-            animated_model.play("walk", 120, dt);
-        }
+    Sprite get_sprite() {
+        int n_frames = this->sprite_sheet.count_frames(this->name);
+        int idx = std::round(this->progress * (n_frames - 1.0));
+        Sprite sprite = this->sprite_sheet.get_sprite(this->name, idx);
+        return sprite;
     }
 };
 
-class World {
-  public:
-    Player player;
+int main() {
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetTargetFPS(60);
+    InitWindow(1024, 768, "Game");
 
-    World(Resources *resources, Vector3 player_position)
-        : player(player_position, resources->animated_models["zed_1"]) {}
+    SpriteSheet sheet("./resources/sprites/atlas.json", "./resources/sprites/atlas.png");
 
-    void update() {
-        if (GetTime() < 0.5) return;
-        player.update(GetFrameTime());
-    }
-};
-
-class Renderer {
-  public:
-    Renderer() {
-        SetConfigFlags(FLAG_MSAA_4X_HINT);
-        SetTargetFPS(60);
-        InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
-        DisableCursor();
-    }
-
-    ~Renderer() {
-        CloseWindow();
-    }
-
-    void draw_world(World &world, Resources &resources) {
-        BeginMode3D(world.player.camera.camera3d);
-
-        Matrix r = QuaternionToMatrix(world.player.transform.rotation);
-        Matrix t = MatrixTranslate(
-            world.player.transform.translation.x,
-            world.player.transform.translation.y,
-            world.player.transform.translation.z
-        );
-        Matrix transform = MatrixMultiply(r, t);
-        rlPushMatrix();
-        rlMultMatrixf(MatrixToFloat(transform));
-
-        world.player.animated_model.draw();
-
-        rlPopMatrix();
-
-        DrawGrid(16, 2.0);
-        EndMode3D();
-        DrawFPS(0, 0);
-    }
-};
-
-int main(void) {
-    Renderer renderer;
-    Resources resources;
-    World world(&resources, Vector3Zero());
+    SpriteSheetAnimator animator(sheet);
+    animator.play("knight_attack_0", 0.1, true);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        world.update();
-        renderer.draw_world(world, resources);
+        animator.update(GetFrameTime());
+        Sprite sprite = animator.get_sprite();
 
         EndDrawing();
     }
-
-    return 0;
 }
---
+++ tools/import_aseprite_sheet.py
@@ -0,0 +1,275 @@
+import json
+import re
+from collections import defaultdict
+from dataclasses import dataclass
+from itertools import chain
+from pathlib import Path
+from typing import Tuple
+
+import cv2
+import numpy as np
+
+_THIS_DIR = Path(__file__).parent
+_ROOT_DIR = _THIS_DIR / ".."
+_ASEPRITE_DIR = _ROOT_DIR / "aseprite"
+_ASSETS_DIR = _ROOT_DIR / "resources/sprites"
+
+_SPRITE_LAYER = "sprite"
+_MASK_LAYER_PREFIX = "mask_"
+
+
+@dataclass
+class Sprite:
+    name: str
+    frame_idx: int
+    image: np.ndarray
+    tl: Tuple[int, int]
+
+    @property
+    def h(self):
+        return self.image.shape[0]
+
+    @property
+    def w(self):
+        return self.image.shape[1]
+
+    @property
+    def tr(self):
+        return (self.tl[0] + self.w, self.tl[1])
+
+    @property
+    def bl(self):
+        return (self.tl[0], self.tl[1] + self.h)
+
+    def get_neighbor_tls(self):
+        bl = self.bl
+        tr = self.tr
+        return [(bl[0], bl[1] + 1), (tr[0] + 1, tr[1])]
+
+    def to_meta(self, sheet_h):
+        # NOTE:
+        # When convertic sprite to the meta (coordinates on the sprite sheet)
+        # we shrink its size by 1 pixel on each size, because the sprite
+        # has been extruded and we need to un-extrude its size back when
+        # saving the sheet meta
+        x, y = self.tl
+        x += 1
+        y += 1
+
+        y = sheet_h - y - 1
+        w, h = self.w, self.h
+        w -= 2
+        h -= 2
+
+        return {
+            "name": self.name,
+            "x": int(x),
+            "y": int(y),
+            "w": int(w),
+            "h": int(h),
+            "frame_idx": int(self.frame_idx),
+        }
+
+
+@dataclass
+class Mask:
+    name: str
+    frame_idx: int
+    tl: Tuple[int, int]
+    w: int
+    h: int
+
+    def to_meta(self, sprite_h):
+        x, y = self.tl
+        y = sprite_h - y - 1
+
+        return {
+            "name": self.name,
+            "x": int(x),
+            "y": int(y),
+            "w": int(self.w),
+            "h": int(self.h),
+            "frame_idx": int(self.frame_idx),
+        }
+
+
+if __name__ == "__main__":
+    out_dir = _ASSETS_DIR
+    out_dir.mkdir(exist_ok=True, parents=True)
+
+    # --------------------------------------------------------------------
+    # Parse aseprite files and extract sprite images from png images
+    sprites = defaultdict(list)
+    masks = defaultdict(lambda: defaultdict(defaultdict))
+    for file_path in _ASEPRITE_DIR.iterdir():
+        if not str(file_path).endswith(".json"):
+            continue
+
+        dir_ = file_path.parent
+        sprite_name = re.sub(r"\.json$", "", file_path.name)
+        meta_fp = dir_ / (sprite_name + ".json")
+        with open(meta_fp) as f:
+            meta = json.load(f)
+        frames = meta["frames"]
+        meta = meta["meta"]
+
+        sheet_fp = dir_ / meta["image"]
+        sheet = cv2.imread(str(sheet_fp), cv2.IMREAD_UNCHANGED)
+        layer_names = [layer["name"] for layer in meta["layers"]]
+        if _SPRITE_LAYER not in layer_names:
+            raise ValueError(f"{meta_fp} is missing the `sprite` layer")
+
+        for frame in frames:
+            name = frame["filename"]
+            sprite_name, layer_name, tag, frame_idx = name.split(".")
+            if tag:
+                sprite_name += f"_{tag}"
+            frame_idx = int(frame_idx)
+
+            is_mask = False
+            if layer_name.startswith(_MASK_LAYER_PREFIX):
+                layer_name = re.sub(r"^mask_", "", layer_name)
+                is_mask = True
+            elif layer_name != _SPRITE_LAYER:
+                raise ValueError(
+                    f"Layer name: {layer_name} is not valid. "
+                    "The layer name should be `sprite` (which represents the actual sprite) "
+                    "or start with `mask_`."
+                )
+
+            x, y, w, h = (frame["frame"][k] for k in ("x", "y", "w", "h"))
+
+            if layer_name == _SPRITE_LAYER:
+                # Expand the sprite frame on 1 pixel on both sides, because
+                # the sheet contains extruded sprites
+                x -= 1
+                y -= 1
+                w += 2
+                h += 2
+
+                image = sheet[y : y + h, x : x + w, :]
+                sprite = Sprite(
+                    name=sprite_name,
+                    frame_idx=frame_idx,
+                    image=image,
+                    tl=(0, 0),
+                )
+                sprites[sprite_name].append(sprite)
+            elif is_mask:
+                image = sheet[y : y + h, x : x + w, :].max(-1)
+                mask = None
+                if image.max() > 0:
+                    row = image.max(0)
+                    col = image.max(1)
+                    left_x = row.argmax()
+                    top_y = col.argmax()
+                    right_x = len(row) - row[::-1].argmax() - 1
+                    bot_y = len(col) - col[::-1].argmax() - 1
+
+                    # Shift the mask on 1 pixel because the corresponding
+                    # sprite has been expanded
+                    # left_x += 1
+                    # right_x += 1
+                    bot_y += 1
+                    top_y += 1
+
+                    w = right_x - left_x + 1
+                    h = bot_y - top_y + 1
+
+                    bot_left = (left_x, bot_y)
+                    top_right = (right_x, top_y)
+                    mask = Mask(
+                        name=sprite_name,
+                        frame_idx=frame_idx,
+                        tl=(left_x, top_y),
+                        w=w,
+                        h=h,
+                    )
+
+                    masks[sprite_name][layer_name][frame_idx] = mask
+            else:
+                assert False, f"Unhandled layer: {layer_name}"
+
+    # --------------------------------------------------------------------
+    # Pack sprites on the sheet
+    flat_sprites = list(chain(*sprites.values()))
+    inds = sorted(
+        range(len(flat_sprites)), key=lambda i: -flat_sprites[i].image.size
+    )
+
+    sheet = np.zeros((0, 0, 5), dtype=np.uint8)
+
+    tls_to_try = [(0, 0)]
+
+    for sprite in flat_sprites:
+        best_tl_idx = None
+        while best_tl_idx is None:
+            for i, tl in enumerate(tls_to_try):
+                min_x, min_y = tl
+                max_x = min_x + sprite.w
+                max_y = min_y + sprite.h
+                if (
+                    sheet.shape[1] > max_x
+                    and sheet.shape[0] > max_y
+                    and sheet[min_y:max_y, min_x:max_x, -1].max() == 0
+                ):
+                    best_tl_idx = i
+                    break
+
+            if best_tl_idx is None:
+                new_sheet = np.zeros(
+                    (
+                        sheet.shape[0] + sprite.h,
+                        sheet.shape[1] + sprite.w,
+                        5,
+                    ),
+                    dtype=np.uint8,
+                )
+                new_sheet[: sheet.shape[0], : sheet.shape[1]] = sheet
+                sheet = new_sheet
+
+        tl = tls_to_try.pop(best_tl_idx)
+        sprite.tl = tl
+
+        min_x, min_y = tl
+        max_x = min_x + sprite.w
+        max_y = min_y + sprite.h
+        sheet[min_y:max_y, min_x:max_x, :-1] = sprite.image
+        sheet[min_y:max_y, min_x:max_x, -1] = 1
+        tls_to_try.extend(sprite.get_neighbor_tls())
+
+    sheet = sheet[..., :-1]
+    # --------------------------------------------------------------------
+    # Prepare sheet meta file (sprites and colliders coordinates)
+    # NOTE: aseprite assumes that the min y is at the top of the sheet,
+    # but I flip it and the 0th y coordinate is at the bottom.
+    # Such the format is more convenient, because it corresponds to the
+    # OpenGL textures coordinates
+    sheet_h, sheet_w = sheet.shape[:2]
+    meta = {
+        "size": [sheet_w, sheet_h],
+        "frames": defaultdict(list),
+    }
+    for sprite_name in sprites:
+        sprites_ = sprites[sprite_name]
+
+        for i in range(len(sprites_)):
+            sprite = sprites_[i]
+            sprite_meta = sprite.to_meta(sheet_h)
+
+            masks_meta = {}
+            for mask_name in masks[sprite_name]:
+                m = masks[sprite_name][mask_name].get(sprite.frame_idx)
+                if m:
+                    masks_meta[mask_name] = m.to_meta(sprite.h)
+
+            frame_meta = {
+                _SPRITE_LAYER: sprite_meta,
+                "masks": masks_meta,
+            }
+            meta["frames"][sprite.name].append(frame_meta)
+
+    # Save the final sheet and meta json
+    cv2.imwrite(str(out_dir / "atlas.png"), sheet)
+    with open(out_dir / "atlas.json", "w") as f:
+        json.dump(meta, f, indent=4)
